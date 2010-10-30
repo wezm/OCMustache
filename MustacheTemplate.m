211df59a5d9e9f95a44da42a9caec8146a459a92
Wesley Moore <wez@wezm.net>
2010-10-30 22:49:52 +1100

    Remove unused code

--- MustacheTemplate.m
+++ MustacheTemplate.m
@@ -136,95 +136,6 @@ static void token_release(CFAllocatorRef allocator, const void *value) {
 	}
 }
 
-//- (void)setRequestMethod:(const char *)at ofLength:(size_t)length
-//{
-//	// no max length validation on this one as the state machine will only read up to 20 chars
-//	NSString *val = [[NSString alloc] initWithBytes:at length:length encoding:NSASCIIStringEncoding];
-//	[http_params setObject:val forKey:global_request_method];
-//	[val release];
-//}
-//
-//- (void)setRequestUri:(const char *)at ofLength:(size_t)length
-//{
-//	validateMaxLength(length, MAX_REQUEST_URI_LENGTH, MAX_REQUEST_URI_LENGTH_ERR);
-//	NSString *val = [[NSString alloc] initWithBytes:at length:length encoding:NSASCIIStringEncoding];
-//	[http_params setObject:val forKey:global_request_uri];
-//	[val release];
-//}
-//
-//- (void)setFragment:(const char *)at ofLength:(size_t)length
-//{
-//	validateMaxLength(length, MAX_FRAGMENT_LENGTH, MAX_FRAGMENT_LENGTH_ERR);
-//	NSString *val = [[NSString alloc] initWithBytes:at length:length encoding:NSASCIIStringEncoding];
-//	[http_params setObject:val forKey:global_fragment];
-//	[val release];
-//}
-//
-//- (void)setRequestPath:(const char *)at ofLength:(size_t)length
-//{
-//	validateMaxLength(length, MAX_REQUEST_PATH_LENGTH, MAX_REQUEST_PATH_LENGTH_ERR);
-//	NSString *val = [[NSString alloc] initWithBytes:at length:length encoding:NSASCIIStringEncoding];
-//	[http_params setObject:val forKey:global_request_path];
-//	[val release];
-//}
-//
-//- (void)setQueryString:(const char *)at ofLength:(size_t)length
-//{
-//	validateMaxLength(length, MAX_QUERY_STRING_LENGTH, MAX_QUERY_STRING_LENGTH_ERR);
-//	NSString *val = [[NSString alloc] initWithBytes:at length:length encoding:NSASCIIStringEncoding];
-//	[http_params setObject:val forKey:global_query_string];
-//	[val release];
-//}
-//
-//- (void)setHttpVersion:(const char *)at ofLength:(size_t)length
-//{
-//	NSString *val = [[NSString alloc] initWithBytes:at length:length encoding:NSASCIIStringEncoding];
-//	[http_params setObject:val forKey:global_http_version];
-//	[val release];
-//}
-//
-//- (void)finaliseHeadersAt:(const char *)at ofLength:(size_t)length
-//{
-//	NSString *content_length = [http_params objectForKey:global_http_content_length];
-//	if(content_length != nil)
-//	{
-//		[http_params setObject:content_length forKey:global_content_length];
-//	}
-//
-//	NSString *content_type = [http_params objectForKey:global_http_content_type];
-//	if(content_type != nil)
-//	{
-//		[http_params setObject:content_type forKey:global_content_type];
-//	}
-//
-//	[http_params setObject:[NSString stringWithString:global_gateway_interface_value] forKey:global_gateway_interface];
-//
-//	// Set host
-//	NSString *http_host = [http_params objectForKey:global_http_host];
-//	if(http_host != nil)
-//	{
-//		// See if a port is already present
-//		NSRange r = [http_host rangeOfString:@":" options:NSLiteralSearch | NSBackwardsSearch];
-//		if(r.location != NSNotFound)
-//		{
-//			// set server name to text upto location
-//			[http_params setObject:[http_host substringToIndex:r.location] forKey:global_server_name];
-//			// TODO: Need to handle the possibility that the colon has nothing after it
-//			[http_params setObject:[http_host substringFromIndex:r.location+1] forKey:global_server_port];
-//
-//		}
-//		else
-//		{
-//			[http_params setObject:http_host forKey:global_server_name];
-//			[http_params setObject:[NSString stringWithString:global_port_80] forKey:global_server_port];
-//		}
-//	}
-//
-//	http_body = [NSMutableData dataWithBytes:at length:length];
-//	[http_params setObject:[NSString stringWithString:global_server_protocol_value] forKey:global_server_protocol];
-//	[http_params setObject:[NSString stringWithString:global_mongrel_version] forKey:global_server_software];
-//}
-
 - (void)reset
 {
 	//	http_parser *http = NULL;
@@ -285,18 +196,6 @@ static void token_release(CFAllocatorRef allocator, const void *value) {
 {
 	return [parser bytesRead];
 }
-
-//- (NSDictionary *)requestParams
-//{
-//	[http_params retain];
-//	return [http_params autorelease];
-//}
-//
-//- (NSData *)requestBody
-//{
-//	[http_body retain];
-//	return [http_body autorelease];
-//}
 
 - (void)dealloc
 {
