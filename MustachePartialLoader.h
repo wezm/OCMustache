b844f2647917dd642cd892f2337bcd30d4f3a79b
Wesley Moore <wez@wezm.net>
2010-11-08 12:59:25 +1100

    Fix up MyCompanyName again

--- MustachePartialLoader.h
+++ MustachePartialLoader.h
@@ -3,7 +3,7 @@
 //  OCMustache
 //
 //  Created by Wesley Moore on 5/11/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2010 Wesley Moore. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
